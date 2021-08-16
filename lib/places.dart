19390b349d7b11a0618cbe70c3c6d5358e3e96d6
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-16 21:20:38 +0545

    Added fields for images

--- lib/places.dart
+++ lib/places.dart
@@ -8,45 +8,75 @@ class ImageSlider extends StatefulWidget {
 class _ImageSliderState extends State<ImageSlider> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          'All',
-          style: TextStyle(
-            color: Color(0xFFED5872),
-            fontSize: 18,
-          ),
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'All',
+                  style: TextStyle(
+                    color: Color(0xFFED5872),
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'America',
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'Europe',
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'Asia',
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'Ocenia',
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                  ),
+                  height: 200,
+                  width: 200,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                  ),
+                  height: 200,
+                  width: 200,
+                ),
+              ],
+            )
+          ],
         ),
-        Text(
-          'America',
-          style: TextStyle(
-            color: Colors.white60,
-            fontSize: 18,
-          ),
-        ),
-        Text(
-          'Europe',
-          style: TextStyle(
-            color: Colors.white60,
-            fontSize: 18,
-          ),
-        ),
-        Text(
-          'Asia',
-          style: TextStyle(
-            color: Colors.white60,
-            fontSize: 18,
-          ),
-        ),
-        Text(
-          'Ocenia',
-          style: TextStyle(
-            color: Colors.white60,
-            fontSize: 18,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
