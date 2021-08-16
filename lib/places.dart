468c36fa1d6357653f9e4c5b3c9b1b67c34371a2
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-16 21:56:37 +0545

    Updated Images

--- lib/places.dart
+++ lib/places.dart
@@ -8,74 +8,87 @@ class ImageSlider extends StatefulWidget {
 class _ImageSliderState extends State<ImageSlider> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'All',
-                  style: TextStyle(
-                    color: Color(0xFFED5872),
-                    fontSize: 18,
-                  ),
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'All',
+                style: TextStyle(
+                  color: Color(0xFFED5872),
+                  fontSize: 18,
                 ),
-                Text(
-                  'America',
-                  style: TextStyle(
-                    color: Colors.white60,
-                    fontSize: 18,
-                  ),
+              ),
+              Text(
+                'America',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 18,
                 ),
-                Text(
-                  'Europe',
-                  style: TextStyle(
-                    color: Colors.white60,
-                    fontSize: 18,
-                  ),
+              ),
+              Text(
+                'Europe',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 18,
                 ),
-                Text(
-                  'Asia',
-                  style: TextStyle(
-                    color: Colors.white60,
-                    fontSize: 18,
-                  ),
+              ),
+              Text(
+                'Asia',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 18,
                 ),
-                Text(
-                  'Ocenia',
-                  style: TextStyle(
-                    color: Colors.white60,
-                    fontSize: 18,
-                  ),
+              ),
+              Text(
+                'Ocenia',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 18,
                 ),
-              ],
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
+              ),
+            ],
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    color: Colors.red,
+                    image: DecorationImage(
+                      image: AssetImage('images/tajmahal.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                height: 200,
+                width: 200,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('images/eifeltower.jpg'),
+                    fit: BoxFit.cover,
                   ),
-                  height: 200,
-                  width: 200,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.red,
-                  ),
-                  height: 200,
-                  width: 200,
-                ),
-              ],
-            )
-          ],
-        ),
+                height: 200,
+                width: 200,
+              ),
+              // Container(
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(20),
+              //     // color: Colors.red,
+              //     image:
+              //         DecorationImage(image: AssetImage('images/everest.jpg')),
+              //   ),
+              //   height: 200,
+              //   width: 200,
+              // ),
+            ],
+          )
+        ],
       ),
     );
   }
